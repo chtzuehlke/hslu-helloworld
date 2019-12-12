73996f1f74a0d661c25036673aa884192e10b8bb
Christian Tschenett <cht@zuehlke.com>
2019-12-12 12:25:58 +0100

    ignore bin

--- helloworld-backup.sh
+++ helloworld-backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tar cvfz helloworld.tgz --exclude='.git' --exclude='target' --exclude='.classpath' --exclude='.settings' --exclude='.project' helloworld/
+tar cvfz helloworld.tgz --exclude='bin' --exclude='.git' --exclude='target' --exclude='.classpath' --exclude='.settings' --exclude='.project' helloworld/
